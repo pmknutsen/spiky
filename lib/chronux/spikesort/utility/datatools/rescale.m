73a031d8ade3910e07767fece4ab57edc90b98ec
pmknutsen <pmknutsen@gmail.com>
2013-10-29 15:48:34 -0700

    Replaced with older version; buggy with hist2d

--- lib/chronux/spikesort/utility/datatools/rescale.m
+++ lib/chronux/spikesort/utility/datatools/rescale.m
@@ -1,15 +1,17 @@
-function [values, oldmin, oldmax] = rescale(values, newmin, newmax)
-%RESCALE           Rescales a data set.
-%   [VALUESOUT, OLDMIN, OLDMAX] = RESCALE(VALUESIN, NEWMIN, NEWMAX), given
-%   a VALUESIN array of arbitrary dimension, will return a VALUESOUT of
-%   the same size and linearly rescaled to the range NEWMIN:NEWMAX.  The
-%   minimum and maximum values of the original matrix are also returned.
-%
-%   Note: if VALUESIN contains only a single value, the scaled output will
-%   be set to (NEWMIN + NEWMAX) / 2.
+function [values, oldmin, oldmax] = rescale(values, newmin, newmax);
 
-extrema = minmax(values);
-oldmin = extrema(1);  oldmax = extrema(2);
+% RESCALE  Rescales a data set.
+%   [VALUES, OLDMIN, OLDMAX] = rescale(VALUES, NEWMIN, NEWMAX)
+% 
+% Given a 'values' input matrix of arbitrary dimension, the VALUES
+% output will be a matrix of the same size linearly rescaled to the
+% range NEWMIN:NEWMAX.  The minimum and maximum values of the
+% original matrix are also returned.
+% Note: if the matrix VALUES contains only a single value, the scaled
+% output will be set to (NEWMIN + NEWMAX) / 2.
+
+oldmin = min(values(:));
+oldmax = max(values(:));
 
 oldrange = oldmax - oldmin;
 newrange = newmax - newmin;
@@ -23,9 +25,11 @@ end
 % Rescaling conceptually uses the following formula:
 %     zero_to_one = (oldvalues - oldmin) ./ oldrange;
 %     newvalues   = (zero_to_one .* newrange) + newmin;
-% But doing it out longhand like this takes two matrix multiplications
-% and two matrix additions.  The command below takes a more convoluted
-% route to do the same thing with half the number of full matrix operations.
+% But doing it out longhand like this takes two matrix
+% multiplications and two matrix additions.  The command
+% below takes a slightly more convoluted route to do the
+% same thing with half the number of full matrix operations.
+
 scale = oldrange ./ newrange;
 shift = oldmin - (scale .* newmin);
 values = (values - shift) ./ scale;
