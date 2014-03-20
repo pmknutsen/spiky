c920ddc8ff5d46c9c383d9b6af613fbfcfde06ea
pmknutsen <pmknutsen@gmail.com>
2014-03-20 15:56:28 -0700

    Completed and tested coherence code for continuous signals.

--- analysis/continuous/Multitaper_Coherence.m
+++ analysis/continuous/Multitaper_Coherence.m
@@ -1,20 +1,21 @@
-function tSig = Multitaper_Coherence(FV)
+function Multitaper_Coherence(FV)
 % Multi taper coherence of two continuous channels.
 %
 % Usage:
 %   S = Multitaper_Coherence(FV)
 %
+% Where S is a structure containing the spectrogram of a selected channel,
+% its sampling rate (kHz) and start/end times (s). Segments that contain
+% NaNs are either filled with mirrors of adjacent data (first) or
+% interpolate (second) before the spectrogram is computed (to avoid edge
+% effects).
+%
 % Requirements:
 %   Chronux spectral analysis library (re-distributed with Spiky)
 %
-% To-do:
-%   Everything!!
-%   Allow coherence of channels with different sampling rate;
-%       i.e. 
 
 global Spiky g_bBatchMode
 persistent p_sContChA p_sContChB p_nMinF p_nMaxF p_nTW p_nD
-tSig = struct([]);
 
 % Select channel A
 if isempty(p_sContChA) || ~g_bBatchMode
@@ -28,27 +29,55 @@ if isempty(p_sContChB) || ~g_bBatchMode
     if ~bResult; return, end
 end
 
-% Fetch data
+% Fetch signal A
 vContA = double(FV.tData.(p_sContChA)');
-% TODO: Get vTime (for downsampling etc below)
 if all(size(vContA) > 1); return; end
-nFsA = FV.tData.([p_sContChA '_KHz']) * 1000;
+nFsA = FV.tData.([p_sContChA '_KHz']) * 1000; % Hz
 nTimeBeginA = FV.tData.([p_sContChA '_TimeBegin']);
+nTimeEndA = FV.tData.([p_sContChA '_TimeEnd']);
 
+% Fetch signal B
 vContB = double(FV.tData.(p_sContChB)');
-% TODO: Get vTime (for downsampling etc below)
 if all(size(vContB) > 1); return; end
-nFsB = FV.tData.([p_sContChB '_KHz']) * 1000;
+nFsB = FV.tData.([p_sContChB '_KHz']) * 1000; % Hz
 nTimeBeginB = FV.tData.([p_sContChB '_TimeBegin']);
+nTimeEndB = FV.tData.([p_sContChB '_TimeEnd']);
+
+% Check that sample rate is same for both channels
+if nFsA > nFsB % decimate A
+    vContA = [flipud(vContA); vContA; flipud(vContA)]; % mirror to avoid edge effects
+    vContA = resample(vContA, round(nFsB), round(nFsA));
+    vContA = vContA(round([length(vContA)/3:[length(vContA)] - length(vContA)/3]));
+    nFs = nFsB;
+elseif nFsA < nFsB % decimate B
+    vContB = [flipud(vContB); vContB; flipud(vContB)]; % mirror to avoid edge effects
+    vContB = resample(vContB, round(nFsA), round(nFsB));
+    vContB = vContB(round([length(vContB)/3:[length(vContB)] - length(vContB)/3]));
+    nFs = nFsA;
+else
+    nFs = nFsA;
+end
+
+% Calculate time vectors
+vTimeA = Spiky.main.GetTime(nTimeBeginA, nTimeEndA, vContA, nFs);
+vTimeB = Spiky.main.GetTime(nTimeBeginB, nTimeEndB, vContB, nFs);
+
+% Force A and B to start at same time and have same length
+% NOTE: NaN's remain NaN's and may produce more NaN's
+nTimeBegin = max([nTimeBeginA nTimeBeginB]);
+nTimeEnd = max([nTimeEndA nTimeEndB]);
+vTime = nTimeBegin:(1/nFs):nTimeEnd;
+vContA = interp1(vTimeA, vContA, vTime, 'linear');
+vContB = interp1(vTimeB, vContB, vTime, 'linear');
 
 % Get parameters interactively
 % We don't collect parameters when function in batch-mode (when known)
 if isempty(p_nMinF) || ~g_bBatchMode
-    if isempty(p_nMinF), p_nMinF = 1; end
-    if isempty(p_nMaxF), p_nMaxF = min(floor([nFsA nFsB]/2.5)); end
+    if isempty(p_nMinF), p_nMinF = max([round(((1/nFs)*2.5)*100)/100 0.01]); end
+    if isempty(p_nMaxF), p_nMaxF = round((nFs/2.5)*100)/100; end
     if isempty(p_nTW), p_nTW = 3; end
     if isempty(p_nD), p_nD = 1; end
-    cPrompt = {'Min frequency (Hz)', 'Max frequency (Hz)', ...
+    cPrompt = {'Minimum frequency (Hz)', 'Maximum frequency (Hz)', ...
         'Time-bandwidth product (TW; s*Hz)', 'Signal derivative'};
     cAnswer = inputdlg(cPrompt,'Options', 1, ...
         {num2str(p_nMinF), num2str(p_nMaxF), ...
@@ -60,57 +89,51 @@ if isempty(p_nMinF) || ~g_bBatchMode
     p_nD = str2double(cAnswer{4});
 end
 
-% Derivative
+% Check parameters
+if p_nMinF == 0
+    warndlg('Minimum frequency must be greater than zero.')
+    return;
+end
+
+% Derivative (whitening)
 if p_nD > 0
     vContA = diff(vContA, p_nD);
     vContB = diff(vContB, p_nD);
 end
 
-% Check that onset time is the same for both signals
-if nTimeBeginA ~= nTimeBeginB
-    warndlg('Both channels need to start at the same time.', 'Multitaper Coherence')
-    return
-end
-
-% Check that sample rate is same for both channels
-if nFsA ~= nFsB
-    % TODO: resample highest Fs trace to lower resolution HERE
-    warndlg('Sampling rate must be same for both channels.', 'Multitaper Coherence')
-    return
-end
-
-% TODO: Fix traces with NaNs HERE
+% Remove NaN's by local mirroring
+vContA = intnans(vContA);
+vContB = intnans(vContB);
 
 % Decimate signal to match user-defined frequency range (speeds up spectral analysis)
 % Note that nFs after decimation must be an integer as there will otherwise
 % be a temporal offset in the output of mtspecgramc (due to internal rounding of nFs)
-if 0
-    nFsO = nFsA;
-    nR_max = floor(nFsA / (p_nMaxF*5)); % scalar must be at least 2.5
+if 1
+    nFsO = nFs;
+    nR_max = floor(nFs / (p_nMaxF*5));
     nR = nR_max;
-    nFsA = nFsO/nR;
-    nFsB = nFsO/nR;
+    nFs = nFsO/nR;
     while 1
         % Check that both nR and nFs are integers
-        if  (ceil(nR) == floor(nR)) && (ceil(nFsA) == floor(nFsA))
+        if  (ceil(nR) == floor(nR)) && (ceil(nFs) == floor(nFs))
             break
         else
             nR = nR - 1;
-            nFsA = nFsO/nR;
-            nFsB = nFsO/nR;
+            nFs = nFsO/nR;
         end
-        if nR == 0 break; end
+        if nR == 0, break; end
     end
-    vContA = decimate(vContA, nR);
-    vContB = decimate(vContB, nR);
-else
-    nFsO = nFsA;
+    if nR > 0
+        vContA = decimate(vContA, nR);
+        vContB = decimate(vContB, nR);
+    else
+        nFs = nFsO;
+    end
 end
 
-
-%% Segment signal vector (to enable trial averaging)
-nSegLen = (1 / (p_nMinF+2)) * nFsA * 2;
-nSegs = floor(length(vContA) / nSegLen)
+% Segment signal vector (to enable trial averaging)
+nSegLen = (1 / (p_nMinF)) * nFs * 2;
+nSegs = floor(length(vContA) / nSegLen);
 mS = [];
 mContA = [];
 mContB = [];
@@ -120,19 +143,19 @@ if nSegs == 0
 else
     for i = 1:nSegs
         vRange = [(i-2)*nSegLen + nSegLen + 1 (i-1)*nSegLen + nSegLen];
-        mContA(:, i) = vContA(vRange(1):vRange(2));
-        mContB(:, i) = vContB(vRange(1):vRange(2));
+        vRange = round(vRange(1):vRange(2));
+        mContA(:, i) = vContA(vRange);
+        mContB(:, i) = vContB(vRange);
     end
 end
 
 % Initialize coherencyc parameters structure
-p_nTW = 5;
 tParams.tapers   = [p_nTW (p_nTW*2)-1]; % [NW K]
 tParams.pad      = 2;
 tParams.err      = [2 .05]; % 0 = no error bars, [1 p] = theoretical error bars, [2 p] = jackknife error
 tParams.fpass    = [p_nMinF p_nMaxF];
 tParams.trialave = 1;
-tParams.Fs       = nFsA;
+tParams.Fs       = nFs;
 
 [C, phi, S12, S1, S2, f, confC, phistd, Cerr] = coherencyc(mContA, mContB, tParams);
 
@@ -166,18 +189,37 @@ hTit = title(hAx(1), sprintf('Multitaper Coherence\n%s vs %s, n=%d trials, %.0f%
     p_sContChA, p_sContChB, nSegs, tParams.err(2).*100), 'color', 'w', 'interpreter', 'none');
 Spiky.main.ThemeObject([hAx hLin hTit])
 
-%%
+return
 
 
-% Error bar estimate
-axes(hAx);
-hFill = fill([f fliplr(f)], [C'+Cerr(1,:) fliplr(C'-Cerr(2,:))], vColHi, 'edgecolor', 'none');
-hLine = plot(hAx, f, C, 'color', vColLo);
 
-Spiky.main.ThemeObject(hTit)
-
-%%
-
+function vCont = intnans(vCont)
+% Interpolate (nearest) indices that contain NaNs
+% Step 1 - NaN segments are filled with mirror images adjacent data
+% Step 2 - Remaining NaNs (e.g. lone occurences, NaNs at start/end of
+%          vector) are interpolated
+iNaN = isnan(vCont(:));
+iTurnsNaN = find([0;diff(iNaN)] == 1) - 1; % start indices of nan segments
+if ~isempty(iTurnsNaN)
+    iNaNDone = find([0;diff(iNaN)] == -1); % end indices of nan segments
+    iNaNDone(iNaNDone <= iTurnsNaN(1)) = [];
+    iTurnsNaN(iTurnsNaN > iNaNDone(end)) = [];
+    iNaNDone = iNaNDone(1:length(iTurnsNaN));
+    for i = 1:length(iTurnsNaN)
+        nL = ceil((iNaNDone(i)-iTurnsNaN(i)+1)/2);
+        if (iTurnsNaN(i)+nL <= length(vCont)) && (iTurnsNaN(i)-nL > 0)
+            vCont(iTurnsNaN(i):(iTurnsNaN(i)+nL)) = fliplr(vCont((iTurnsNaN(i)-nL):iTurnsNaN(i))');
+        end
+        if ((iNaNDone(i)-nL) > 0) && (iNaNDone(i)+nL <= length(vCont))
+            vCont((iNaNDone(i)-nL):iNaNDone(i)) = fliplr(vCont(iNaNDone(i):(iNaNDone(i)+nL))');
+        end
+    end
+    % Interpolate remaining NaNs
+    iNaNb = isnan(vCont);
+    if any(iNaNb)
+        vCont(iNaNb) = interp1(find(~iNaNb), vCont(~iNaNb), find(iNaNb), 'linear');
+    end
+end
 return
 
 
