1b3bc6d12bff347e972d50142edb4e74c5c9d46c
pmknutsen <p.m.knutsen@medisin.uio.no>
2017-01-05 11:41:12 +0100

    New  helpline

--- extensions/Sample_Extension.m
+++ extensions/Sample_Extension.m
@@ -1,9 +1,13 @@
-function FV = Test_Script(FV)
+function FV = Sample_Extension(FV)
 % Just a test of the scripting functionality in Spiky!
 %
 % In your own scripts, you can read and modify the content in FV. If your
 % script returns an error, set FV.ScriptError to contain a descriptive
 % string to be displayed on the command line.
+%
+% Script files must be prefixed with spiky_. For example:
+%   spiky_my_script.m
+%
 %
 
 warndlg('This test script does nothing.', 'Spiky!')
